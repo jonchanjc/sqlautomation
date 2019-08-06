6cce94bc417197e6100614fdc7e88abccefd9d5a
Jonathan Chan <jon.chanjc@gmail.com>
2019-08-06 15:48:05 -0400

    Add document into script

--- src/Get-MxAgReplicaStatus.ps1
+++ src/Get-MxAgReplicaStatus.ps1
@@ -14,4 +14,23 @@ function Get-MxAgReplicaStatus
         $sql=Get-SqlInstance -ServerInstance $ComputerName
         ($sql.AvailabilityGroups).AvailabilityReplicas | Where-Object {$_.Role -ne "UNKNOWN"} | Select-Object @{N='AvailabilityGroup';E={$_.Parent}}, @{N='ReplicaName';E={$_.Name}}, Role, RollupSynchronizationstate, FailoverMode, AvailabilityMode 
     }
+
+<#
+.SYNOPSIS
+
+Return AG replica status
+
+.DESCRIPTION
+
+List all the availability groups, replica names and their corresponding roles, failover mode and synchronization status on any AG replica.
+This eliminates the bottleneck from waiting for DBA to provide the information.
+
+.PARAMETER ComputerName
+The server name of the SQL Server replica
+
+.EXAMPLE
+
+Get-MxAgReplicaStatus -ComputerName SQLV-URTSI05
+#>
+
 }
