95eecd1baa112c19d260dec51d1bf577eb318ea1
Jonathan Chan <jon.chanjc@gmail.com>
2019-08-07 10:15:01 -0400

    Update to allow sql login

--- src/Get-MxAgReplicaStatus.ps1
+++ src/Get-MxAgReplicaStatus.ps1
@@ -6,12 +6,20 @@ function Get-MxAgReplicaStatus
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory=$true)]
-        [string] $ComputerName
+        [string] $ComputerName,
+        [Parameter(Mandatory=$false)]
+        [pscredential] $Credential
     )
 
     Process
     {
-        $sql=Get-SqlInstance -ServerInstance $ComputerName
+        if ($Credential) {
+            $sql=Get-SqlInstance -ServerInstance $ComputerName -Credential $Credential
+        }
+        else {
+            $sql=Get-SqlInstance -ServerInstance $ComputerName
+        }
+        
         ($sql.AvailabilityGroups).AvailabilityReplicas | Where-Object {$_.Role -ne "UNKNOWN"} | Select-Object @{N='AvailabilityGroup';E={$_.Parent}}, @{N='ReplicaName';E={$_.Name}}, Role, RollupSynchronizationstate, FailoverMode, AvailabilityMode 
     }
 
